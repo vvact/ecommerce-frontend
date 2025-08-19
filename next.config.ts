35ba26ac90c04d9b628d547d7dbc63f54923ffb7
vvact <vbm7752@gmail.com>
2025-08-19 06:22:13 +0300

    new

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: ['res.cloudinary.com'], // allow Cloudinary images
+    remotePatterns: [
+      {
+        protocol: "http",
+        hostname: "res.cloudinary.com",
+        domains: ['res.cloudinary.com'], // add your Cloudinary domain
+        pathname: "/dizhnvo43/**",
+      },
+    ],
   },
 };
 
