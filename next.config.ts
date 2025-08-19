989726937c9ed772175265f04145dfc7098f37b6
vvact <vbm7752@gmail.com>
2025-08-19 21:56:12 +0300

    new commit

--- next.config.ts
+++ next.config.ts
@@ -1,14 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    remotePatterns: [
-      {
-        protocol: "http",
-        hostname: "res.cloudinary.com",
-        domains: ['res.cloudinary.com'], // add your Cloudinary domain
-        pathname: "/dizhnvo43/**",
-      },
-    ],
+    domains: ["res.cloudinary.com"], // ✅ allow all Cloudinary images
   },
 };
 
